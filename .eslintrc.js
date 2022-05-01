6b5847ff257ebf4f89be745de010819770767e2a
InSeong-So <goflvhxj2547@gmail.com>
2022-05-01 19:22:18 +0900

    🔬Chore: 프로젝트 셋업
    - yarn-berry, mono-repo 구성
    - lint, prettier 등 설정
    - 팀원들의 에디터 환경 공유를 위해 .idea 추가

---
+++ .eslintrc.js
@@ -0,0 +1,98 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'plugin:@next/next/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint', 'react-hooks', '@emotion', 'testing-library'],
+  // common rules
+  rules: {
+    'prettier/prettier': 'error',
+    // typescript
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    // next
+    '@next/next/no-html-link-for-pages': 'off',
+    // react
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-useless-fragment': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx', '.jsx'] }],
+    'react/function-component-definition': 'off',
+    'jsx-a11y/label-has-associated-control': ['error', { controlComponents: ['input', 'select'] }],
+    // import options
+    'sort-imports': 'off',
+    'import/prefer-default-export': 'off',
+    'import/order': 'off',
+    'import/no-duplicates': 'off',
+    'import/no-default-export': 'off',
+    'import/no-named-as-default': 'off',
+    'no-restricted-exports': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+    //
+    'max-params': ['error', 3],
+  },
+  // special rules
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      rules: {
+        'no-use-before-define': 'off',
+        'no-useless-constructor': 'off',
+        'react/prop-types': 'off',
+        'react/require-default-props': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
+        '@typescript-eslint/no-useless-constructor': 'error',
+        '@typescript-eslint/no-floating-promises': 'off',
+      },
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 12,
+        sourceType: 'module',
+        project: ['./tsconfig.json', './apps/**/tsconfig.json', './packages/**/tsconfig.json'],
+      },
+    },
+    {
+      files: ['apps/01-infinite-scroll/**/*.ts?(x)', 'apps/01-infinite-scroll/**/*.js?(x)'],
+      settings: {
+        'import/resolver': {
+          typescript: {
+            project: path.resolve(`${__dirname}/apps/01-infinite-scroll/tsconfig.json`),
+          },
+        },
+      },
+    },
+  ],
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts', '.js', '.jsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+};

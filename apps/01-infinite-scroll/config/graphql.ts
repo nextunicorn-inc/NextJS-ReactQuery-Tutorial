502494177c2c9cef94240cef2733d0021f26f387
Steven Semyung Oh <semyung.dev@gmail.com>
2022-08-08 14:11:24 +0900

    [CHORE] 안쓰는 코드 정리

--- apps/01-infinite-scroll/config/graphql.ts
+++ apps/01-infinite-scroll/config/graphql.ts
@@ -1,5 +1,3 @@
 import { GraphQLClient } from 'graphql-request';
-const URL = 'https://beta.pokeapi.co/graphql/v1beta';
-// const URL = 'https://beta.pokeapi.co/graphql/v1beta';
 const client = new GraphQLClient('https://beta.pokeapi.co/graphql/v1beta');
 export default client;
